a6172fef148f92122482a19209d8feb785fd551c
Huseyn Ismayilzada <hismayilzada06@gmail.com>
2025-04-29 07:44:12 -0700

    Update GameRunner.java

--- GameRunner.java
+++ GameRunner.java
@@ -1,15 +1,38 @@
 import utilities.GDV5;
+
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class GameRunner extends GDV5 {
 
+    private Snake snake = new Snake(20);
+
     @Override
     public void update() {
+        if (KeysPressed[KeyEvent.VK_UP]) {
+            snake.setDirection(Snake.Direction.UP);
+        }
+        if (KeysPressed[KeyEvent.VK_DOWN]) {
+            snake.setDirection(Snake.Direction.DOWN);
+        }
+        if (KeysPressed[KeyEvent.VK_LEFT]) {
+            snake.setDirection(Snake.Direction.LEFT);
+        }
+        if (KeysPressed[KeyEvent.VK_RIGHT]) {
+            snake.setDirection(Snake.Direction.RIGHT);
+        }
 
+        snake.move();
     }
+
 
     @Override
     public void draw(Graphics2D win) {
+        snake.draw(win);
+    }
 
+    public static void main(String[] args) {
+        GameRunner game = new GameRunner();
+        game.start();
     }
 }
